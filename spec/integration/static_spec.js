b2801aad16d7454a8694c14560d976bebabfbe55
diva-D <davidgentile@gmail.com>
2019-02-08 10:08:46 -0500

    Assignment routing... completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -4,11 +4,23 @@ const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
     
+    // Index page
     describe("GET /", () => {
         it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
                 expect(body).toContain("Welcome to Bloccit");
+            });
+        });
+    });
+
+    // About page
+    describe("GET /about", () => {
+        it("should return status code 200 and have 'About Us' in the body of the response", () => {
+            const about = base + "about/";
+            request.get(about, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("About Us");
             });
         });
     });
