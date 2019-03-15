269354893e6f83d14d35578a8241c132e167eaf0
diva-D <davidgentile@gmail.com>
2019-03-14 21:31:09 -0400

    checkpoint comments completed

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -4,6 +4,7 @@ module.exports = {
         const topicRoutes = require("../routes/topics");
         const userRoutes = require("../routes/users");
         const postRoutes = require("../routes/posts");
+        const commentRoutes = require("../routes/comments");
 
         if(process.env.NODE_ENV === "test") {
             const mockAuth = require("../../spec/support/mock-auth.js");
@@ -11,6 +12,7 @@ module.exports = {
         }
 
         app.use(userRoutes);
+        app.use(commentRoutes);
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(postRoutes);
